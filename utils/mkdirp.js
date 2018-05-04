cffe9e8c89738bc04197533743cb21cb878bb2e1
ngx-devtools <aelbore.sg@gmail.com>
2018-05-04 16:44:27 +0800

    updates

--- utils/mkdirp.js
+++ utils/mkdirp.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 const memoize = require('./memoize');
 
 const mkdirp = (directory) => {
-  const dirPath = directory.replace(/\/$/, '').split('/');
+  const dirPath = directory.replace(/\/$/, '').split(path.sep);
   for (let i = 1; i <= dirPath.length; i++) {
-    const segment = dirPath.slice(0, i).join('/');
+    const segment = dirPath.slice(0, i).join(path.sep);
     if (!fs.existsSync(segment) && segment.length > 0) {
       fs.mkdirSync(segment);
     }
