7820c48c7a080ef40f19259db9c13896d112bde5
ngx-devtools <aelbore.sg@gmail.com>
2018-07-19 09:37:54 +0800

    minify options should be optional

--- src/minify.ts
+++ src/minify.ts
@@ -2,11 +2,11 @@ import { readFileAsync } from './file';
 
 const terser = require('terser');
 
-async function minifyContent(content: string, options: any) {
-  return Promise.resolve(terser.minify(content, options));
+async function minifyContent(content: string, options?: any) {
+  return Promise.resolve(terser.minify(content, options || {}));
 }
 
-async function minify(file: string, options: any) {
+async function minify(file: string, options?: any) {
   return readFileAsync(file, 'utf8').then(content => {
     return minifyContent(content, options);
   })
